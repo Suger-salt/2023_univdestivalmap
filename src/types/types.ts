7a4261da78d19c4156d5900ae10b1a9c247abb19
Suger-salt <u120018@st.pu-toyama.ac.jp>
2023-09-29 21:47:53 +0900

    add. 商品を複数表示できるようにした

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,9 +1,28 @@
-export type dataTypes = {
-  Products: {
-    productImage: string;
-    productName: string;
-    productPrice: number;
-  };
+// export type dataTypes = {
+//   Products: {
+//     productImage: string;
+//     productName: string;
+//     productPrice: number;
+//   };
+//   Shop: {
+//     shopDetail: string;
+//     shopImage: string;
+//     shopName: string;
+//   };
+//   User: {
+//     clubName: string;
+//   };
+// };
+
+// タイプを追加
+export type Product = {
+  productImage: string;
+  productName: string;
+  productPrice: number;
+};
+
+export type ShopData = {
+  Products: Product[];
   Shop: {
     shopDetail: string;
     shopImage: string;
