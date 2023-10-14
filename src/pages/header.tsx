75eb269e501e3096cd6e1f7e78b5968036c7bb3e
Suger-salt <u120018@st.pu-toyama.ac.jp>
2023-10-15 01:04:15 +0900

    add. stage parts fix. overlay

--- src/pages/header.tsx
+++ src/pages/header.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import styles from "@/styles/menu.module.css";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -7,24 +8,34 @@ const Header = () => {
     setMenuOpen(!menuOpen);
   };
 
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <>
       <div className="w-full bg-[#3598DB] h-[8vh]  mb-4  relative">
         <div
-          className="absolute left-[10px] top-1/2 transform -translate-y-1/2 cursor-pointer"
+          className={`absolute right-[10px] top-1/2 transform -translate-y-1/2 cursor-pointer ${
+            styles.hamburger
+          } ${menuOpen ? styles.active : ""}`}
           onClick={toggleMenu}
         >
-          遷移
+          <span className={styles.line}></span>
+          <span className={styles.line}></span>
+          <span className={styles.line}></span>
         </div>
         <div className="absolute right-[43%] top-1/2 transform -translate-y-1/2 ">
           <img src="/images/icon_POLYGON.svg" alt="Icon" />
         </div>
       </div>
 
+      {menuOpen && <div className={styles.overlay} onClick={closeMenu}></div>}
+
       {menuOpen && (
-        <div className="fixed top-0 left-0 w-64 h-screen bg-white shadow p-4">
-          <ul className="list-none">
-            <li className="mb-2">
+        <div className="fixed top-[8vh] left-0 w-[30vw] h-screen bg-[#333] shadow p-4 z-10 bg-opacity-90">
+          <ul className="list-none text-white">
+            <li className="mb-2 ">
               <a href="/#">topPage</a>
             </li>
             <li className="mb-2">
