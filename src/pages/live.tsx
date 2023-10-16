04a2117ac10ac9efb46d5ab8babb6db05fe4316b
Suger-salt <u120018@st.pu-toyama.ac.jp>
2023-10-16 14:43:19 +0900

    fix. liveSchedule

--- src/pages/live.tsx
+++ src/pages/live.tsx
@@ -1,11 +1,20 @@
 import Header from "./header";
+import styles from "@/styles/live.module.css";
 
 const Live = () => {
   return (
     <>
       <Header />
-      <div>ここはライブページです</div>
-      <img src="/images/live1.svg" />
+
+      <div className={styles.text}>
+        食堂では以下のスケジュールで
+        <br />
+        ライブが行われています！！
+      </div>
+
+      <div className="m-[1rem]">
+        <img src="/images/liveSchedule.svg" />
+      </div>
     </>
   );
 };
