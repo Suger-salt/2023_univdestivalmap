294c60645302601176a7558ccab0a5598a3f4538
Suger-salt <u120018@st.pu-toyama.ac.jp>
2023-10-17 03:23:28 +0900

    make_stagePage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,19 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import {
+  ThemeProvider,
+  StyledEngineProvider,
+  CssBaseline,
+} from "@mui/material";
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <StyledEngineProvider injectFirst>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </StyledEngineProvider>
   );
 }
